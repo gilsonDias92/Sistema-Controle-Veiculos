db12ae87569e72e84979e8e5bf476296a7c8cf04
gilsonDias92 <gilsinho.1992@gmail.com>
2019-09-30 15:14:09 -0300

    Inicio do projeto

---
+++ ControleVeiculos2019/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ControleVeiculos2019.Startup))]
+namespace ControleVeiculos2019
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
